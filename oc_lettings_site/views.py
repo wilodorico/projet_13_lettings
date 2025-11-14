505ee656452fc8a9fa7051d172ad548d81ebb257
wilfried <wilfried.odorico@gmail.com>
2025-11-14 11:47:46 +0100

    Move view profiles to app profiles

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Letting, Profile
+from .models import Letting
 
 
 def index(request):
@@ -20,15 +20,3 @@ def letting(request, letting_id):
         "address": letting.address,
     }
     return render(request, "letting.html", context)
-
-
-def profiles_index(request):
-    profiles_list = Profile.objects.all()
-    context = {"profiles_list": profiles_list}
-    return render(request, "profiles_index.html", context)
-
-
-def profile(request, username):
-    profile = Profile.objects.get(user__username=username)
-    context = {"profile": profile}
-    return render(request, "profile.html", context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from profiles.models import Profile
+
+
+def profiles_index(request):
+    profiles_list = Profile.objects.all()
+    context = {"profiles_list": profiles_list}
+    return render(request, "profiles_index.html", context)
+
+
+def profile(request, username):
+    profile = Profile.objects.get(user__username=username)
+    context = {"profile": profile}
+    return render(request, "profile.html", context)
