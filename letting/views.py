6f6d127c8b707779b1d4572dd6904b314f6e53a4
wilfried <wilfried.odorico@gmail.com>
2025-11-14 13:46:38 +0100

    move views in new app letting

--- letting/views.py
+++ letting/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from letting.models import Letting
+
+
+def lettings_index(request):
+    lettings_list = Letting.objects.all()
+    context = {"lettings_list": lettings_list}
+    return render(request, "lettings_index.html", context)
+
+
+def letting(request, letting_id):
+    letting = Letting.objects.get(id=letting_id)
+    context = {
+        "title": letting.title,
+        "address": letting.address,
+    }
+    return render(request, "letting.html", context)
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,22 +1,5 @@
 from django.shortcuts import render
 
-from .models import Letting
-
 
 def index(request):
     return render(request, "index.html")
-
-
-def lettings_index(request):
-    lettings_list = Letting.objects.all()
-    context = {"lettings_list": lettings_list}
-    return render(request, "lettings_index.html", context)
-
-
-def letting(request, letting_id):
-    letting = Letting.objects.get(id=letting_id)
-    context = {
-        "title": letting.title,
-        "address": letting.address,
-    }
-    return render(request, "letting.html", context)
