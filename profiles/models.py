af85b52b0a6a49122004f6a3bc948619e2eed1d8
wilfried <wilfried.odorico@gmail.com>
2025-11-14 11:26:19 +0100

    Add Profile model in app profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_city = models.CharField(max_length=64, blank=True)
+
+    class Meta:
+        db_table = "profiles_profile"
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
+
+    def __str__(self):
+        return self.user.username
