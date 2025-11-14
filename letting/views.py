2a185b22ed9d716385dfabce071f39d1fe938801
wilfried <wilfried.odorico@gmail.com>
2025-11-14 14:35:17 +0100

    rename template to index and update link url

--- letting/views.py
+++ letting/views.py
@@ -6,7 +6,7 @@ from letting.models import Letting
 def index(request):
     lettings_list = Letting.objects.all()
     context = {"lettings_list": lettings_list}
-    return render(request, "letting/lettings_index.html", context)
+    return render(request, "letting/index.html", context)
 
 
 def letting(request, letting_id):
