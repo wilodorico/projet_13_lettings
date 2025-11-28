5b1ea73aa0291e3f04a81434e601dc7eab85094e
wilfried <wilfried.odorico@gmail.com>
2025-11-28 14:50:35 +0100

    refactor tests: remove unnecessary database marker from model tests

--- letting/tests/test_models.py
+++ letting/tests/test_models.py
@@ -10,20 +10,17 @@ def sample_address():
     )
 
 
-@pytest.mark.django_db
 def test_address_str(sample_address):
     address = sample_address
 
     assert str(address) == "10 Place de la victoire"
 
 
-@pytest.mark.django_db
 def test_letting_str(sample_address):
     letting = Letting(title="Charming Flat", address=sample_address)
     assert str(letting) == "Charming Flat"
 
 
-@pytest.mark.django_db
 def test_letting_address_relation(sample_address):
     address = sample_address
     letting = Letting(title="Sunny Apartment", address=address)
