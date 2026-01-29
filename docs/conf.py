28efcac08c99f6bb1728c70dd3978ce5109001ed
wilfried <wilfried.odorico@gmail.com>
2026-01-29 15:38:44 +0100

    fix: Remove extra closing brace in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -4,38 +4,37 @@ import sys
 
 import django
 
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'oc_lettings_site.settings.local'
+sys.path.insert(0, os.path.abspath(".."))
+os.environ["DJANGO_SETTINGS_MODULE"] = "oc_lettings_site.settings.local"
 django.setup()
 
 # -- Project information -----------------------------------------------------
-project = 'OC Lettings'
-copyright = '2026, Orange County Lettings'
-author = 'Orange County Lettings'
-release = '1.0'
+project = "OC Lettings"
+copyright = "2026, Orange County Lettings"
+author = "Orange County Lettings"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-language = 'fr'
+language = "fr"
 
 # -- Options for HTML output -------------------------------------------------
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 html_logo = None
 html_favicon = None
 
 # -- Options for autodoc -----------------------------------------------------
 autodoc_default_options = {
-    'members': True,
-    'undoc-members': True,
-    'show-inheritance': True,
-}
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
 }
