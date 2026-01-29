ca0de08b5b09c045d7d941ddea9e48e40c553fb7
wilfried <wilfried.odorico@gmail.com>
2026-01-29 16:01:43 +0100

    fix: Remove django.setup() from Sphinx conf to fix Read The Docs build

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,4 @@
 # Configuration file for the Sphinx documentation builder.
-import os
-import sys
-
-import django
-
-sys.path.insert(0, os.path.abspath(".."))
-os.environ["DJANGO_SETTINGS_MODULE"] = "oc_lettings_site.settings.local"
-django.setup()
 
 # -- Project information -----------------------------------------------------
 project = "OC Lettings"
